5cae0ea7eead3e447fb566511cc11b64cbaf6580
asasuzu <133328222+asasuzu@users.noreply.github.com>
2023-10-10 15:19:09 +0000

    fix_swiper8

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,13 +3,10 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import Swiper from 'swiper/swiper-bundle.js';
-import 'swiper/swiper-bundle.css';
 
 Rails.start()
 Turbolinks.start()
