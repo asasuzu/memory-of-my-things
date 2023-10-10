30497c119cdf475cefb833ad3d58aec94111ec83
asasuzu <133328222+asasuzu@users.noreply.github.com>
2023-10-10 11:25:08 +0000

    fix_swiper3

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "jquery"
 import Swiper from 'swiper/swiper-bundle.js';
 import 'swiper/swiper-bundle.css';
 
