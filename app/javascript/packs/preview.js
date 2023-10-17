976815c8f898cf00bfc9f4ddc506b33a97571d8d
asasuzu <133328222+asasuzu@users.noreply.github.com>
2023-10-17 06:53:58 +0000

    fix_preview.js

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -3,28 +3,43 @@ document.addEventListener("turbolinks:load", function() {
   const imagePreview = document.getElementById('image-preview');
   const fileNamePreview = document.getElementById('file-name-preview');
 
-  fileInput.addEventListener('change', function (event) {
+  fileInput.addEventListener('change', handleFileInputChange);
+
+  function handleFileInputChange(event) {
     const file = event.target.files[0];
     if (file) {
       const fileExtension = file.name.split('.').pop().toLowerCase();
       const allowedExtensions = ["jpeg", "jpg", "png", "gif"];
 
       if (allowedExtensions.includes(fileExtension) && file.type.startsWith("image/")) {
-        const reader = new FileReader();
-        reader.onload = function (e) {
-          imagePreview.src = e.target.result;
-          imagePreview.style.display = 'block';
-          fileNamePreview.innerText = file.name;
-        };
-        reader.readAsDataURL(file);
+        displayPreview(file);
       } else {
-        // 許可されていないファイル形式の場合の処理
-        alert("画像ファイル（jpegまたはpng）を選択してください。");
-        fileInput.value = '';
-        imagePreview.src = '';
-        imagePreview.style.display = 'none';
-        fileNamePreview.innerText = '';
+        showError("画像ファイル（jpegまたはpng）を選択してください.");
+        clearPreview();
       }
+    } else {
+      clearPreview();
     }
-  });
+  }
+
+  function displayPreview(file) {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      imagePreview.src = e.target.result;
+      imagePreview.style.display = 'block';
+      fileNamePreview.innerText = file.name;
+    };
+    reader.readAsDataURL(file);
+  }
+
+  function showError(message) {
+    alert(message);
+    fileInput.value = '';
+  }
+
+  function clearPreview() {
+    imagePreview.src = '';
+    imagePreview.style.display = 'none';
+    fileNamePreview.innerText = '';
+  }
 });
